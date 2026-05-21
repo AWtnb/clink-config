4fe9fca0ce265fdd27303ae3fd45d024244d9eb8
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-05-21 14:13:19 +0900

    update

--- commands.lua
+++ commands.lua
@@ -1,4 +1,10 @@
 function Forward_bigword_with_space(rl_buffer)
+    if rl_buffer:getbuffer() == "" then return end
+
+    local pos = rl_buffer:getcursor()
+    local len = rl_buffer:getlength()
+
+    if pos ~= len + 1 then return end
     rl.invokecommand("clink-forward-bigword")
     rl_buffer:insert(" ")
 end
